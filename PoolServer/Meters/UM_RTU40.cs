bbcc2f34a284032ffaa8067153b5646bfb2760b6
ivan khr <i.d.khromov@gmail.com>
2017-04-18 01:19:13 +0300

    вернул обратно

--- PoolServer/Meters/UM_RTU40.cs
+++ PoolServer/Meters/UM_RTU40.cs
@@ -600,7 +600,7 @@ namespace Prizmer.Meters
 
             string answ = ASCIIEncoding.ASCII.GetString(incommingData);
 
-            WriteToLog(answ);
+            WriteToLog("getDailyValuesForID: " + answ);
 
             List<string> recordStringsForDates = new List<string>();
 
@@ -918,16 +918,16 @@ namespace Prizmer.Meters
             WriteToLog("Begin to read daily: ");
             if (listOfDailyValues == null || listOfDailyValues.Count == 0)
             {
-               // if (!getDailyValuesForID(meterId, dt, out listOfDailyValues))
-                if (!getDailyValuesForID(meterId, out listOfDailyValues))
+                if (!getDailyValuesForID(meterId, dt, out listOfDailyValues))
+               // if (!getDailyValuesForID(meterId, out listOfDailyValues))
                 {
                     WriteToLog("getDailyValuesForID returned false ");
                     return false;
                 } 
             }
 
-            //string paramName = dailyCorrelationDict[param];
-            string paramName = currCorrelationDict[param];
+            string paramName = dailyCorrelationDict[param];
+            //string paramName = currCorrelationDict[param];
             string fullParamName = paramName + tarif.ToString();
             
             ValueUM val = new ValueUM();
