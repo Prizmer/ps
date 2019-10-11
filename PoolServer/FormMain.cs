8c162900cda33600a867b11acad1a62a5391a078
idkhromov <i.d.khromov@gmail.com>
2019-10-11 22:31:14 +0300

    Пульсар. Если у прибора нельзя было определить версию или версии не было в перечислении, то принималось за дефолт - вода 485. Это приводило к неправильной конвертации, а следовательно 0м в лучшем случае. Сейчас в случае, в описаном случае - драйвер возвращает false и в базе это выглядит как NA.

--- PoolServer/FormMain.cs
+++ PoolServer/FormMain.cs
@@ -95,7 +95,7 @@ namespace Prizmer.PoolServer
            // byte[] cmdHang = ASCIIEncoding.ASCII.GetBytes(at_cmd_hang);
             
 
-            const string SO_VERSION = "v. 0.30.5";
+            const string SO_VERSION = "v. 0.30.6";
             this.Text += " - " + SO_VERSION;
 
             try
