6c6632e46d92b40094260b8a444d4f15f9270926
idkhromov <i.d.khromov@gmail.com>
2019-11-01 00:01:17 +0300

    Прописал определения для модели счетчика teplo_v9 = 0x0140

--- PoolServer/FormMain.cs
+++ PoolServer/FormMain.cs
@@ -95,7 +95,7 @@ namespace Prizmer.PoolServer
            // byte[] cmdHang = ASCIIEncoding.ASCII.GetBytes(at_cmd_hang);
             
 
-            const string SO_VERSION = "v. 0.31.2";
+            const string SO_VERSION = "v. 0.31.3";
             this.Text += " - " + SO_VERSION;
 
             try
